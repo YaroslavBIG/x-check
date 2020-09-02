75a9c73b7ceacd3ad9b91eacf1a174a8b3615709
Chekhuta Dmytro <chehuta1996@gmail.com>
2020-09-02 23:42:16 +0300

    feat: create basic data-structure

--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -1,12 +1,91 @@
 import React, {Component} from 'react';
 import "./Tasks"
 
-export default class Tasks extends Component{
+import {Table} from 'antd';
+
+const dataSource = [
+    {
+        key: '1',
+        task_name: 'X-Check App',
+        status: 'Published',
+        due_data: '16/08',
+        requests: 9177,
+        author: 'Evan Flores',
+        max_score: 640,
+        actions: '...'
+    },
+    {
+        key: '2',
+        task_name: 'X-Check App',
+        status: 'Published',
+        due_data: '16/08',
+        requests: 9177,
+        author: 'Evan Flores',
+        max_score: 640,
+        actions: '...'
+    },
+    {
+        key: '3',
+        task_name: 'X-Check App',
+        status: 'Published',
+        due_data: '16/08',
+        requests: 9177,
+        author: 'Evan Flores',
+        max_score: 640,
+        actions: '...'
+    },
+];
+
+
+const columns = [
+    {
+        title: 'Task Name',
+        dataIndex: 'task_name',
+        key: 'task_name',
+    },
+    {
+        title: 'Status',
+        dataIndex: 'status',
+        key: 'status',
+    },
+    {
+        title: 'Due Data',
+        dataIndex: 'due_data',
+        key: 'due_data',
+    },
+    {
+        title: '#requests',
+        dataIndex: 'requests',
+        key: 'requests',
+    },
+    {
+        title: 'Author',
+        dataIndex: 'author',
+        key: 'author',
+    },
+    {
+        title: 'Max Score',
+        dataIndex: 'max_score',
+        key: 'max_score',
+    },
+    {
+        title: 'Actions',
+        dataIndex: 'actions',
+        key: 'actions',
+    },
+];
+
+class Tasks extends Component {
+
+
     render() {
+
         return (
-            <div>
-                <h1>Tasks</h1>
-            </div>
-        )
+            <>
+                <Table dataSource={dataSource} columns={columns}/>
+            </>
+        );
     }
 }
+
+export default Tasks;
