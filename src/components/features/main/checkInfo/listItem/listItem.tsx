94f3b73b40d10fef75bcd6dd70ec964f55b7ff06
missukevitch <missukevitch@gmail.com>
2020-09-08 23:44:49 +0300

    fix: change path

--- src/components/features/main/checkInfo/listItem/listItem.tsx
+++ src/components/features/main/checkInfo/listItem/listItem.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react';
-import styles from './listItem.module.scss';
+import styles from './ListItem.module.scss';
 
 interface ListItemProps {
   heading: string,
