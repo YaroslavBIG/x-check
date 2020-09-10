bc860850888dc71cbffd305f219bc361ea58b2ff
missukevitch <missukevitch@gmail.com>
2020-09-10 22:08:44 +0300

    feat: add check info form

--- src/components/features/main/Requests/Requests.tsx
+++ src/components/features/main/Requests/Requests.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Form } from 'antd';
+import { EditOutlined } from '@ant-design/icons';
+import CheckInfo from '../CheckInfo/CheckInfo';
 
 export const Requests = () => {
+    const [isVisible, setVisibility] = useState(false);
+    const [form] = Form.useForm();
+
+    const onClose = () => {
+        setVisibility(false);
+        form.resetFields();
+    }
+
     return (
         <div>
             <h1>Requests</h1>
+            <Button icon={<EditOutlined/>} onClick={() => setVisibility(true)}>Show check info form</Button>
+            <CheckInfo isVisible={isVisible} onClose={onClose} form={form}/>
         </div>
     );
 };
