080479705be02b7db40044603a571770ca70bd6a
Maksim <maksim-nemtsev@users.noreply.github.com>
2020-09-18 20:08:06 +0300

    feat: delete console.log

--- src/components/features/main/Requests/TableRequests/TableRequests.tsx
+++ src/components/features/main/Requests/TableRequests/TableRequests.tsx
@@ -47,7 +47,6 @@ const TableRequests = () => {
       .get()
       .then((query) => {
         query.forEach((doc) => {
-          // console.log(doc.data());
           requests = [...requests, transformRequests(doc.data())];
         });
         setRequests(requests);
@@ -58,7 +57,6 @@ const TableRequests = () => {
   const [selectedRowKeys, setSelectedRowKeys] = useState([]);
 
   const onSelectChange = (selectedRowKeys: any) => {
-    // console.log('selectedRowKeys changed: ', selectedRowKeys);
     setSelectedRowKeys(selectedRowKeys);
   };
 
