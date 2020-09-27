65f8ec522941536bf6a607752355cd22c830964b
Maksim <maksim-nemtsev@users.noreply.github.com>
2020-09-28 01:04:30 +0300

    feat: add requests context feat: add buttons (delete, edit)

--- src/components/features/main/Requests/TopPanelRequests/TopPanelRequests.tsx
+++ src/components/features/main/Requests/TopPanelRequests/TopPanelRequests.tsx
@@ -1,84 +1,84 @@
-import React, { useContext, useState } from 'react';
-import styles from './TopPanelRequests.module.scss';
-import { Button, Form } from 'antd';
-import 'antd/dist/antd.css';
-import { DeleteOutlined, EditOutlined, ExclamationCircleOutlined, InfoCircleOutlined } from '@ant-design/icons';
-import RequestForm from '../../RequestForm/RequestForm';
-import confirm from 'antd/lib/modal/confirm';
-import { toast } from 'react-toastify';
-import { useFirestore } from 'react-redux-firebase';
-import { RequestsContext } from '../RequestsContext/RequestsContext';
+import React, { useContext, useState } from "react";
+import styles from "./TopPanelRequests.module.scss";
+import { Button, Form } from "antd";
+import "antd/dist/antd.css";
+import {
+  DeleteOutlined,
+  EditOutlined,
+  ExclamationCircleOutlined,
+  InfoCircleOutlined,
+} from "@ant-design/icons";
+import RequestForm from "../../RequestForm/RequestForm";
+import confirm from "antd/lib/modal/confirm";
+import { toast } from "react-toastify";
+import { useFirestore } from "react-redux-firebase";
+import { RequestsContext } from "../RequestsContext/RequestsContext";
 
 const TopPanelRequests = () => {
-	const [ isVisible, setVisibility ] = useState(false);
-  const [ form ] = Form.useForm();
-  const {
-        selectedRequests,
-        setSelectedRequests,
-				} = useContext(RequestsContext)
+  const [isVisible, setVisibility] = useState(false);
+  const [form] = Form.useForm();
+  const { selectedRequests, setSelectedRequests } = useContext(RequestsContext);
 
   const updFirestore = useFirestore();
 
   const deleteFromFirebase = async (docName: string) => {
     try {
-      updFirestore.collection('requests').doc(docName).delete()
+      updFirestore.collection("requests").doc(docName).delete();
     } catch (error) {
-      toast.error(error)
+      toast.error(error);
     }
-  }
-
-  const deleteDocs = async (array: string[]) => {
-  for (const item of array) {
-    await deleteFromFirebase(item);
-  }
-    toast.success('Request deleted')
-  }
-
-  const showConfirm = () => {
-		confirm({
-			title: 'Delete Sessions',
-			icon: <ExclamationCircleOutlined />,
-			content: 'Are you sure you want to delete the selected sessions?',
-			onOk() {
-				if(selectedRequests?.length){
-          deleteDocs(selectedRequests)
-        }
-        setSelectedRequests([]);
-			}
-		});
-	}
-
-
-
-	const handleClose = () => {
-		setVisibility(false);
-		form.resetFields();
   };
 
-	return (
-		<div className={styles.topPanelRequests__container}>
-			<div className={styles.topPanelRequests__crud}>
-				<Button
-					danger
-					icon={<DeleteOutlined />}
-					disabled={false}
-					className={'tasks-header--button'}
-					onClick={showConfirm}
-				/>
-				{/* <Button type="default"><DeleteOutlined className={styles.topPanelRequests__crud__delete} /></Button> */}
-				<EditOutlined className={styles.topPanelRequests__crud__edit} />
-				<Button
-					className={styles.topPanelRequests__crud__btn}
-					onClick={() => setVisibility(true)}
-					type='primary'
-				>
-					Create
-				</Button>
-				<RequestForm isVisible={isVisible} onClose={handleClose} form={form} />
-				<InfoCircleOutlined className={styles.topPanelRequests__crud__info} />
-			</div>
-		</div>
-	);
+  const deleteDocs = async (array: string[]) => {
+    for (const item of array) {
+      await deleteFromFirebase(item);
+    }
+    toast.success("Request deleted");
+  };
+
+  const showConfirm = () => {
+    confirm({
+      title: "Delete Sessions",
+      icon: <ExclamationCircleOutlined />,
+      content: "Are you sure you want to delete the selected sessions?",
+      onOk() {
+        if (selectedRequests?.length) {
+          deleteDocs(selectedRequests);
+        }
+        setSelectedRequests([]);
+      },
+    });
+  };
+
+  const handleClose = () => {
+    setVisibility(false);
+    form.resetFields();
+  };
+
+  return (
+    <div className={styles.topPanelRequests__container}>
+      <div className={styles.topPanelRequests__crud}>
+        {" "}
+        <Button
+          icon={<EditOutlined />}
+          className={styles.topPanelRequests__crud__edit}
+        >
+          Edit Request
+        </Button>
+        <Button
+          danger
+          icon={<DeleteOutlined />}
+          disabled={false}
+          className={"tasks-header--button"}
+          onClick={showConfirm}
+        >
+          Delete Request
+        </Button>
+        <RequestForm isVisible={isVisible} onClose={handleClose} form={form} />
+        {/* <InfoCircleOutlined className={styles.topPanelRequests__crud__info} /> */}
+      </div>
+    </div>
+  );
 };
 
 export default TopPanelRequests;
