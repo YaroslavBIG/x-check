dbc0dcfdc736dc37340e839ac82570a41f0926a0
Maksim <maksim-nemtsev@users.noreply.github.com>
2020-09-27 22:00:04 +0300

    fix: add buttons

--- src/components/features/main/Requests/TopPanelRequests/TopPanelRequests.tsx
+++ src/components/features/main/Requests/TopPanelRequests/TopPanelRequests.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
-import styles from './TopPanelRequests.module.scss';
-import { Button } from 'antd';
-import 'antd/dist/antd.css';
-import { DeleteOutlined, LockOutlined, EditOutlined, InfoCircleOutlined } from '@ant-design/icons';
+import React from "react";
+import styles from "./TopPanelRequests.module.scss";
+import { Button } from "antd";
+import "antd/dist/antd.css";
+import {
+  DeleteOutlined,
+  EditOutlined,
+  InfoCircleOutlined,
+} from "@ant-design/icons";
 
 const TopPanelRequests = () => {
   return (
     <div className={styles.topPanelRequests__container}>
       <div className={styles.topPanelRequests__crud}>
-        <DeleteOutlined className={styles.topPanelRequests__crud__delete} />
-        <LockOutlined className={styles.topPanelRequests__crud__lock} />
-        <EditOutlined className={styles.topPanelRequests__crud__edit} />
         <Button className={styles.topPanelRequests__crud__btn} type="primary">
           Create
+        </Button>
+        <Button
+          icon={<EditOutlined />}
+          className={styles.topPanelRequests__crud__edit}
+        >
+          Edit
+        </Button>
+        <Button
+          danger
+          icon={<DeleteOutlined />}
+          className={styles.topPanelRequests__crud__delete}
+        >
+          Delete
         </Button>
         <InfoCircleOutlined className={styles.topPanelRequests__crud__info} />
       </div>
